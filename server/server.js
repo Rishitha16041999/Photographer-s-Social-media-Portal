e0e5f9bb2aaf01f717d79fa86318ccda370eab74
KathirvelChandrasekaran <kathirvel98@outlook.com>
2022-06-05 06:20:59 +0530

    feat: fetched users who are opt to freelance

--- server/server.js
+++ server/server.js
@@ -4,35 +4,38 @@ const express = require("express");
 const fs = require("fs");
 
 const morgan = require("morgan");
-const { register} = require("./controllers/auth");
-const { login } = require('./controllers/LoginAuth');
+const { register } = require("./controllers/auth");
+const { login } = require("./controllers/LoginAuth");
 require("dotenv").config();
 
 const app = express();
 
 //DB
-mongoose.connect(process.env.DATABASE, {
+mongoose
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
-    
-    useUnifiedTopology: true
-  
-}).then(() => console.log("DB Connected"))
-.catch((err) => console.log("DB CONNECTION ERROR =>", err));
+
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB CONNECTION ERROR =>", err));
 
 //Middlewares
 
-app.use(express.json({limit: "5mb"}));
-app.use(express.urlencoded({extended: true}));
-app.use(cors({
-    origin: ["http://localhost:3000"],
-})
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+  })
 );
 
 //Autoload routes
 
 //fs.readdirSync("./routes").map((r) => app.use("/api", require(`.routes/${r}`)) );
-app.use("/api/", require("./routes/auth"))
-app.use("/api/", require("./routes/post"))
+app.use("/api/", require("./routes/auth"));
+app.use("/api/", require("./routes/post"));
+app.use("/api/", require("./routes/hire"));
 
 const port = process.env.PORT || 8000;
-app.listen(port, () =>console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
